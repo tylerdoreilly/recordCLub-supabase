0b8a5a51ed82381cc47a16d7f8041d7f9000c248
tylerdoreilly <tylerdoreilly@gmail.com>
2022-10-21 17:55:41 -0400

    setup for supabase

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCts9YXeGsHPWtRviuqspVaiv6ZY37LtLI",
+  authDomain: "record-club-7deae.firebaseapp.com",
+  databaseURL: "https://record-club-7deae-default-rtdb.firebaseio.com",
+  projectId: "record-club-7deae",
+  storageBucket: "record-club-7deae.appspot.com",
+  messagingSenderId: "119903254637",
+  appId: "1:119903254637:web:ed465da3b4fb64b2392764",
+  measurementId: "G-VL3GN7MDXV"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
